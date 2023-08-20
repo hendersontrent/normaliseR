e4039a237d374989eaeaae3705453dafa85d15a2
Trent <trent.henderson1@outlook.com>
2023-08-20 15:57:14 +1000

    initial build

---
+++ tests/testthat/test-normaliseR.R
@@ -0,0 +1,18 @@
+
+x <- stats::rnorm(100)
+
+test_that("zScore works", {
+  expect_equal(length(normalise(x, norm_method = "zScore")), length(x))
+})
+
+test_that("MinMax works", {
+  expect_equal(length(normalise(x, norm_method = "MinMax")), length(x))
+})
+
+test_that("Sigmoid works", {
+  expect_equal(length(normalise(x, norm_method = "Sigmoid")), length(x))
+})
+
+test_that("RobustSigmoid works", {
+  expect_equal(length(normalise(x, norm_method = "RobustSigmoid")), length(x))
+})
